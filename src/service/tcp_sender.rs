7d910a1b4bf2bbbd51135dbb9b5d35b37354567d
Pereki <ruben.elias.kraft@outlook.de>
2026-04-12 15:31:56 +0200

    feat: add println when message was sent

--- src/service/tcp_sender.rs
+++ src/service/tcp_sender.rs
@@ -6,9 +6,10 @@ impl TcpSender {
     pub fn send_message<T: AsRef<str>>(message: T, addr: T) -> Result<(), std::io::Error> {
         println!("Sending message: '{}' to address: '{}'", message.as_ref(), addr.as_ref());
         
-        
         let mut stream = TcpStream::connect(addr.as_ref())?;
         stream.write(message.as_ref().as_bytes())?;
+
+        println!("Message sent successfully.");
         Ok(())
     }
 } 
